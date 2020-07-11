9ed650adc18d9844397324f9a4d7a7709b8141d2
Divino Rodrigues <divino.sr@hotmail.com>
2020-07-11 20:51:07 -0300

    Creating named routes

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
-import '../screens/categories_meals_screen.dart';
+import '../utils/app_routes.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   CategoryItem(this.category);
 
-  void _selectCategory(BuildContext context) {}
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context).settings.arguments as Category;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Receitas'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Receitas por Categoria'),
+        child: Text('Receitas por Categoria ${category.id}'),
       ),
     );
   }
